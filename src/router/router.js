d65dbdba9574baae32453c7f08a91c8a1cb8860d
liuxiaosan333 <43360349+liuxiaosan333@users.noreply.github.com>
2018-11-13 09:16:16 +0800

    liuxiaosan333

--- src/router/router.js
+++ src/router/router.js
@@ -67,7 +67,6 @@ export const appRouter = [
 			{ path: 'userlist', title: '用户管理', name: 'user-info',icon: 'compose',component: () => import('@/views/system/user/user-manage.vue') },
 			{ path: 'orglist', title: '机构管理', name: 'org_info', icon: 'compose',component: () => import('@/views/system/org/sys-org.vue') },
 			{ path: 'parmlist', title: '系统参数', name: 'parm_info',icon: 'compose',component: () => import('@/views/system/parm/sys-parm.vue') },
-			{ path: 'testlist', title: '测试', name: 'test_info',icon: 'compose',component: () => import('@/views/system/test/test.vue') },
 			{
 				path:'hardware',
 				icon: 'android-checkbox',
