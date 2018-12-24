d5c4bb029e682c793b3318311f2934aa23878636
zhuyongchao <1451339508@qq.com>
2018-12-24 11:39:52 +0800

    zhuyongchao
    12.24 am

--- src/views/system/business/systemDefinition/unit/businessUnit_column.js
+++ src/views/system/business/systemDefinition/unit/businessUnit_column.js
@@ -19,19 +19,18 @@ businessUnit.getColumns = function() {
 	return [
 				{ 
 					type: 'selection',
+					width: 60,
 			        align: 'center'
 			    },
 			    {
 					title: '单元名称',
 			        key: 'unitName',
-					width: 150,
 			        align: 'center'
 			    },
 				{
 			        title: '关联表',
 			        key: 'relTable',
 			        align: 'center',
-					width: 150,
 					render: (h, params) => {
 					  return h('div', [
 						 h('span', {
@@ -53,7 +52,6 @@ businessUnit.getColumns = function() {
 			        title: '关联字段',
 			        key: 'relColumn',
 			        align: 'center',
-					width: 150,
 					render: (h, params) => {
 					  return h('div', [
 						 h('span', {
